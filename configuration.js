c9f3c409182e0b65b76b85f673d8056523ac5545
jim-sion <sion.jim@protonmail.com>
2024-11-05 15:27:20 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -3,17 +3,16 @@ const config = {
   description: {
     EN: {
       short: `Jim Sion is a young social scientist working on agri-food changes in Reunion island, 
-      currently a Université Paris Cité PhD student. He is affiliated to the <a href="https://umr-lisis.fr/membre/jim-sion/" target="> _blank
-      Laboratoire Interdisciplinaire Sciences Innovations Sociétés (LISIS)</a>, and to
-      the <a href="https://www.ceped.org/fr/membres/Doctorants-77/article/sion-jim" target=">Centre Population et Développement (CEPED). `,
+      currently a Université Paris Cité PhD student. He is affiliated to the <a href="https://umr-lisis.fr/membre/jim-sion/" target="_blank">Laboratoire Interdisciplinaire Sciences Innovations Sociétés (LISIS)</a>, and to
+      the <a href="https://www.ceped.org/fr/membres/Doctorants-77/article/sion-jim" target="_blank">>Centre Population et Développement (CEPED). `,
       long: "",
     },
     FR: {
       short: `Jim Sion est un jeune chercheur en sciences sociales spécialisé sur les transformations agri-alimentaires à La Réunion,
       actuellement doctorant à l'Université Paris Cité. 
-     Il est affilié au <a href="https://umr-lisis.fr/membre/jim-sion/" target=">
+     Il est affilié au <a href="https://umr-lisis.fr/membre/jim-sion/" target="_blank">
       Laboratoire Interdisciplinaire Sciences Innovations Sociétés (LISIS)</a>, 
-      et au <a href="https://www.ceped.org/fr/membres/Doctorants-77/article/sion-jim" target=">Centre Population et Développement (CEPED). `,
+      et au <a href="https://www.ceped.org/fr/membres/Doctorants-77/article/sion-jim" target="_blank">Centre Population et Développement (CEPED). `,
       long: "",
     },
   },
